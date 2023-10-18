d69e1d8b3ba53d6c128674b84ce0b805fdb27bdf
Harthley01 <rm551359@fiap.com.br>
2023-10-18 12:06:26 -0300

    AULA18

---
+++ src/app/produtos/mosfet/[slug]/page.jsx
@@ -0,0 +1,9 @@
+
+export default function ProdutoSlug({params}) {
+  return (
+    <div>
+        <h1>Produto Slug</h1>
+        <p>o valor do produto Slug: {params.Slug}</p>
+    </div>
+  )
+}
---
+++ src/app/produtos/nike/[id]/page.jsx
@@ -0,0 +1,10 @@
+
+export default function ProdutoID({params}) {
+    return (
+      <div>
+          <h1>Produto ID</h1>
+          <p>o valor do produto Slug: {params.id}</p>
+      </div>
+    )
+  }
+  
